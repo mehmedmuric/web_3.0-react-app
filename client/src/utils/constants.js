ab653e551b0afe85971ec0c7e95d018f313e805a
Mehmed Muric <mehmedmuric22@gmail.com>
2023-10-21 20:06:56 +0200

    contract address

---
+++ client/src/utils/constants.js
@@ -0,0 +1,17 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+
+
+export const contractAddress = ' 0x9dE72E9ebD89BE60AE3d4a796CBD787e829016b3';
+
+
+
+
+
+
+
+
+
+
+
